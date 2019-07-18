88b8587731a1fccc1ccc82fc3d683a5c54540acb
Leo Borquez <ele.dev@protonmail.com>
2019-07-17 23:43:12 -0400

    Calling api
    get the info of the citys

--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,18 @@
 const key = '5OQJFkAvVhtqOkaQEOT5h0austbGMBYS';
 
+//get weather information
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -8,8 +21,15 @@ const getCity = async (city) => {
     const response = await fetch(base + query);
     const data = await response.json();
 
-    console.log(data);
+    return (data[0]); // closest match
 
 };
 
-getCity('miami');
+// first get cuty then data weather via data.Key then log the data
+getCity('miami').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data)
+})
+.catch(err => console.log(err));
+
