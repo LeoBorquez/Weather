5f4c4774d7425cba8e448e74dd6631cd6d1799ba
Leo Borquez <ele.dev@protonmail.com>
2019-07-17 23:28:10 -0400

    calling Api acuWeather

---
+++ js/forecast.js
@@ -0,0 +1,15 @@
+const key = '5OQJFkAvVhtqOkaQEOT5h0austbGMBYS';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+
+};
+
+getCity('miami');
