042b674f3b1b27120082ae900604785df63ac541
Leo Borquez <ele.dev@protonmail.com>
2019-07-18 23:41:42 -0400

    Show dataUI
    html template

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
 const updateCity = async (city) => {
 
@@ -6,7 +8,7 @@ const updateCity = async (city) => {
     const weather = await getWeather(cityDetails.Key);
 
     return {
-        cityDetails : cityDetails, //const equal object property
+        cityDetails: cityDetails, //const equal object property
         weather: weather
     };
 };
@@ -21,6 +23,22 @@ cityForm.addEventListener('submit', e => {
 
     // update the ui with new city
     updateCity(city)
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 });
+
+const updateUI = (data) => {
+
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    // update template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+};
